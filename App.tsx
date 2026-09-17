2d1b2ab4827babe656185a12b2b1be84b7a0fb4f
JhonnyyUrb <jhonny_urbina@unitec.edu>
2026-09-17 16:22:20 -0600

    agregar soporte de idiomas español e ingles

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { View, ActivityIndicator, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux'; // <-- 1. Importar Redux Provider
-
+import { LanguageProvider } from './src/context/LanguageContext';
 import { store } from './src/store'; // <-- 2. Importar el store creado
 import { BottomNav } from './src/components/BottonNav';
 import { FavoritesProvider } from './src/context/FavoritesContext';
@@ -28,7 +28,8 @@ const RootNavigator = () => {
 
 export default function App() {
   return (
-    <Provider store={store}> {/* <-- 3. Envolver la app con el Provider de Redux */}
+    <Provider store={store}> 
+      <LanguageProvider>
       <SafeAreaProvider>
         <AuthProvider>
           <FavoritesProvider>
@@ -40,6 +41,7 @@ export default function App() {
           </FavoritesProvider>
         </AuthProvider>
       </SafeAreaProvider>
+       </LanguageProvider>
     </Provider>
   );
 }
